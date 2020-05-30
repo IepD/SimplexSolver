d62b9f4fc4df6cbed4470a09b4f7b837f9c21d7e
Lucas Dias <lucasdias@univem.edu.br>
2020-05-30 17:12:30 -0300

    Implementação da apresentação da Análise de Sensibilidade.

--- Controllers/SimplexController.cs
+++ Controllers/SimplexController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SimplexSolver.Models;
@@ -17,6 +18,7 @@ namespace SimplexSolver.Controllers
         int qtdInterations;
         decimal[] funcaoObjetiva;
         decimal[,] matrizNumerica;
+        QuadroSimplex quadroSimplexOriginal;
         string[] Var;
         string[] ElementosOriginais;
         List<QuadroSimplex> Resultado;
@@ -37,6 +39,7 @@ namespace SimplexSolver.Controllers
                 if (!PreValidateFields(matrizNumerica))
                     RedirectToAction("Index", "Home");
             Run();
+            AnaliseSensibilidade();
             return View(Resultado);
         }
 
@@ -109,6 +112,7 @@ namespace SimplexSolver.Controllers
             ElementosOriginais = (string[])Var.Clone();
 
             quadroSimplex = new QuadroSimplex(Var, matrizNumerica, funcaoObjetiva);
+            quadroSimplexOriginal = quadroSimplex.Clone(quadroSimplex);
         }
 
         public void Run()
@@ -218,9 +222,211 @@ namespace SimplexSolver.Controllers
                     Results.Add(new Result() { 
                         Elemento = quadroSimplex.Elementos[i], 
                         Valor = i < numeroRestricoes ? quadroSimplex.Matriz[i, qtdColunas - 1] : 
-                        (i == numeroRestricoes + qtdColunas - 1 ? quadroSimplex.FuncaoObjetiva[qtdColunas - 1] : 0) });
+                        (i == numeroRestricoes + qtdColunas - 1 ? (Minimizar ? -quadroSimplex.FuncaoObjetiva[qtdColunas - 1] : quadroSimplex.FuncaoObjetiva[qtdColunas - 1]) : 0) });
             }
+
             ViewData["Resultado"] = Results;
+        }
+
+        public void AnaliseSensibilidade()
+        {
+            string[] ResultadoAnaliseSensibilidade = new string[(numeroRestricoes + numeroVariaveis + 1) * 14];
+            int aumentarParametro = 0;
+            int reduzirParametro = 0;
+            for (int i = 0, k = 0; i < (numeroRestricoes + numeroVariaveis + 1); i++)
+            {
+                for (int j = 0; j < 14; j++, k++)
+                {
+                    switch (j)
+                    {
+                        #region Primeira Coluna
+                        case 0:
+                            ResultadoAnaliseSensibilidade[k] = ElementosOriginais[i + numeroRestricoes];
+                            break;
+                        #endregion
+
+                        #region Segunda Coluna
+                        case 1:
+                            if(i < numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = "Decisão";
+                            else if (i == (numeroRestricoes + numeroVariaveis + 1))
+                                ResultadoAnaliseSensibilidade[k] = "Função Objetivo";
+                            else
+                                ResultadoAnaliseSensibilidade[k] = "Folga";
+                            break;
+                        #endregion
+
+                        #region Terceira Coluna
+                        case 2:
+                            if (i == (numeroRestricoes + numeroVariaveis))
+                                ResultadoAnaliseSensibilidade[k] = Math.Round(quadroSimplexOriginal.FuncaoObjetiva[qtdColunas - 1], 2).ToString();
+                            else
+                                if (i >= numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = Math.Round(quadroSimplexOriginal.Matriz[i - numeroVariaveis, qtdColunas - 1], 2).ToString();
+                            else
+                                ResultadoAnaliseSensibilidade[k] = "0";
+                            break;
+                        #endregion
+
+                        #region Quarta Coluna
+                        case 3:
+                            ResultadoAnaliseSensibilidade[k] = Math.Round(Results.Where(x => x.Elemento.Equals(ElementosOriginais[i + numeroRestricoes])).FirstOrDefault().Valor, 2).ToString();
+                            break;
+                        #endregion
+
+                        #region Quinta Coluna
+                        case 4:
+                            bool isBasic = false;
+                            for(int y = 0; y < numeroRestricoes; y++)
+                            {
+                                if(ElementosOriginais[i + numeroRestricoes].Equals(quadroSimplex.Elementos[y]) || i == (numeroRestricoes + numeroVariaveis))
+                                {
+                                    ResultadoAnaliseSensibilidade[k] = "Sim";
+                                    isBasic = true;
+                                    break;
+                                }
+                            }
+                            if(!isBasic)
+                                ResultadoAnaliseSensibilidade[k] = "Não";
+                            break;
+                        #endregion
+
+                        #region Sexta Coluna
+                        case 5:
+                            if (i < numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else if (i == (numeroRestricoes + numeroVariaveis))
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else
+                                ResultadoAnaliseSensibilidade[k] = (Results.Where(x => x.Elemento.Equals(ElementosOriginais[i + numeroRestricoes])).FirstOrDefault().Valor == 0 ? "Sim" : "Não");
+                        break;
+                        #endregion
+
+                        #region Sétima Coluna
+                        case 6:
+                            if (i < numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else if (i == (numeroRestricoes + numeroVariaveis))
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else
+                                ResultadoAnaliseSensibilidade[k] = Math.Round(Results.Where(x => x.Elemento.Equals(ElementosOriginais[i + numeroRestricoes])).FirstOrDefault().Valor, 2).ToString();
+                            break;
+                        #endregion
+
+                        #region Oitava Coluna
+                        case 7:
+                            if (i < numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else if (i == (numeroRestricoes + numeroVariaveis))
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else
+                                ResultadoAnaliseSensibilidade[k] = (Convert.ToDecimal(ResultadoAnaliseSensibilidade[k - 5]) - Convert.ToDecimal(ResultadoAnaliseSensibilidade[k - 4])).ToString();
+                            break;
+                        #endregion
+
+                        #region Nona Coluna
+                        case 8:
+                            if (i < numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else if (i == (numeroRestricoes + numeroVariaveis))
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else
+                                ResultadoAnaliseSensibilidade[k] = Math.Round(quadroSimplex.FuncaoObjetiva[i], 2).ToString();
+                            break;
+                        #endregion
+
+                        #region Décima Coluna
+                        case 9:
+                            if (i < numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = Math.Round(quadroSimplex.FuncaoObjetiva[i], 2).ToString();
+                            else
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            break;
+                        #endregion
+
+                        #region Décima Primeira Coluna
+                        case 10:
+                            decimal aux = -1;
+                            decimal resultaux = 0;
+                            if (i < numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else if (i == (numeroRestricoes + numeroVariaveis))
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else
+                            {
+                                for(int y = 0; y < numeroRestricoes; y++)
+                                {
+                                    if (quadroSimplex.Matriz[y, numeroVariaveis + aumentarParametro] == 0)
+                                        continue;
+
+                                    resultaux = (quadroSimplex.Matriz[y, qtdColunas - 1] / quadroSimplex.Matriz[y, numeroVariaveis + aumentarParametro]) * -1;
+
+                                    if ((resultaux < aux && resultaux >= 0) || (aux == -1 && resultaux >= 0))
+                                        aux = resultaux;
+                                }
+                                aumentarParametro++;
+                                ResultadoAnaliseSensibilidade[k] = (aux != -1 ? Math.Round(aux, 2).ToString() : "INFINITO");
+                            }
+                            break;
+                        #endregion
+
+                        #region Décima Segunda Coluna
+                        case 11:
+                            decimal aux2 = 1;
+                            decimal resultaux2 = 0;
+                            if (i < numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else if (i == (numeroRestricoes + numeroVariaveis))
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else
+                            {
+                                for (int y = 0; y < numeroRestricoes; y++)
+                                {
+                                    if (quadroSimplex.Matriz[y, numeroVariaveis + reduzirParametro] == 0)
+                                        continue;
+
+                                    resultaux2 = (quadroSimplex.Matriz[y, qtdColunas - 1] / quadroSimplex.Matriz[y, numeroVariaveis + reduzirParametro]) * -1;
+
+                                    if ((resultaux2 > aux2 && resultaux2 <= 0) || (aux2 == 1 && resultaux2 <= 0))
+                                        aux2 = resultaux2;
+                                }
+                                reduzirParametro++;
+                                ResultadoAnaliseSensibilidade[k] = (aux2 != 1 ? Math.Round(aux2 * -1, 2).ToString() : "INFINITO");
+                            }
+                            break;
+                        #endregion
+
+                        #region Décima Terceira Coluna
+                        case 12:
+                            if (i < numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else if (i == (numeroRestricoes + numeroVariaveis))
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else
+                                if (ResultadoAnaliseSensibilidade[k - 10].Equals("INFINITO") || ResultadoAnaliseSensibilidade[k - 2].Equals("INFINITO"))
+                                    ResultadoAnaliseSensibilidade[k] = "INFINITO";
+                                else
+                                    ResultadoAnaliseSensibilidade[k] = (Convert.ToDecimal(ResultadoAnaliseSensibilidade[k - 10]) + Convert.ToDecimal(ResultadoAnaliseSensibilidade[k - 2])).ToString();
+                            break;
+                        #endregion
+
+                        #region Décima Quarta Coluna
+                        case 13:
+                            if (i < numeroVariaveis)
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else if (i == (numeroRestricoes + numeroVariaveis))
+                                ResultadoAnaliseSensibilidade[k] = "-";
+                            else
+                                if (ResultadoAnaliseSensibilidade[k - 11].Equals("INFINITO") || ResultadoAnaliseSensibilidade[k - 2].Equals("INFINITO"))
+                                    ResultadoAnaliseSensibilidade[k] = "INFINITO";
+                                else
+                                    ResultadoAnaliseSensibilidade[k] = (Convert.ToDecimal(ResultadoAnaliseSensibilidade[k - 11]) - Convert.ToDecimal(ResultadoAnaliseSensibilidade[k - 2])).ToString();
+                            break;
+                            #endregion
+                    }
+                }
+            }
+            ViewData["Analise"] = ResultadoAnaliseSensibilidade;
         }
 
         public int FindPositionLowestValue(decimal[] funcaoObjetiva)
